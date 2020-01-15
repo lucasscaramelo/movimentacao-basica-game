72a4baba002758694ff3986bc247d1473bd73884
Lucas Gabriel Scaramelo <lucas.scaramelo@FROMTIS.LOCAL>
2020-01-15 13:23:35 -0300

    insert functions

--- script.js
+++ script.js
@@ -7,6 +7,35 @@
     background.src = "scene.png";
 
 
+    //objetos
+    var sprites = [];
+    var gameWorld = {
+        img: background,
+        x: 0,
+        y: 0,
+        width: 800,
+        height: 600
+    };
+    sprites.push(gameWorld);
+
+    var cam = {
+        x: 0,
+        y: 0,
+        width: cnv.width,
+        height: cnv.height,
+        leftEdge: function(){
+            return this.x + (this.width * 0.25);
+        },
+        rightEdge: function(){
+            return this.x + (this.width * 0.75);
+        },
+        topEdge: function(){
+            return this.x + (this.width * 0.25);
+        },
+        bottomEdge: function(){
+            return this.x + (this.width * 0.75);
+        }
+    }
 
     function loop(){
         window.requestAnimationFrame(loop, cnv);
@@ -19,7 +48,12 @@
     }
     
     function render(){
-        ctx.drawImage(background, 0, 0, 800, 600, 0, 0, 800, 600);
+        ctx.save();
+        ctx.translate(-cam.x, -cam.y);
+        for(var i in sprites){
+            var spr = sprites[i];
+            ctx.drawImage(spr.img, 0, 0, spr.width, spr.height, spr.x, spr.y, spr.width, spr.height);
+        }
     }
 
     loop();
