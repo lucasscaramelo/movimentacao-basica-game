4894995309cab6beb1340636aa9df61a2e14f233
Lucas Gabriel Scaramelo <lucas.scaramelo@FROMTIS.LOCAL>
2020-01-17 10:56:06 -0300

    new function

--- script.js
+++ script.js
@@ -117,6 +117,20 @@
             char.y += 2;
         }
 
+        //limite char
+        if(char.x < 0){
+            char.x = 0;
+        }
+        if(char.x + char.width > gameWorld.width){
+            char.x = gameWorld.width - char.width;
+        }
+        if(char.y < 0){
+            char.y = 0;
+        }
+        if(char.y + char.height > gameWorld.height){
+            char.y = gameWorld.height - char.height;
+        }
+
         //atualiza posição cam
         if(char.x < cam.leftEdge()){
             cam.x = char.x - (cam.width * 0.25);
@@ -124,6 +138,28 @@
         if(char.x + char.width > cam.RightEdge()){
             cam.x = char.x + char.width - (cam.width * 0.75);
         }
+        if(char.y < cam.topEdge()){
+            cam.y = char.y - (cam.height * 0.25);
+        }
+        if(char.y + char.height > cam.bottomEdge()){
+            cam.y = char.y + char.height - (cam.height * 0.75);
+        }   
+    
+        //limite cam
+        if(cam.x < 0){
+            cam.x = 0;
+        }
+        if(cam.x + cam.width > gameWorld.width){
+            cam.x = gameWorld.width - cam.width;
+        }
+        if(cam.y < 0){
+            cam.y = 0;
+        }
+        if(cam.y + cam.height > gameWorld.height){
+            cam.y = gameWorld.height - cam.height;
+        }
+
+
     }
       
     
@@ -134,6 +170,9 @@
             var spr = sprites[i];
             ctx.drawImage(spr.img, 0, 0, spr.width, spr.height, spr.x, spr.y, spr.width, spr.height);
         }
+        ctx.restore();
+        ctx.font = "bold 25px Arial";
+        ctx.fillText("SCORE: 0", 10, 30)
     }
 
     loop();
